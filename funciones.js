2daab98239650136343adad1961ee8006e3f04af
cdcarcamo <carlosdcarcamos@gmail.com>
2024-05-19 21:31:36 -0500

    Registrando front con el servidor configurado

---
+++ funciones.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+    this.documentElementById('enlace_pagina_pedidos').addEventListener('click', function(){
+        window.location.href = 'pedidos.html'
+    })
+})
